42ff4af98515cf9da5a621cf9ecb154b79104535
RogueClaris <seventh_sage@hotmail.com>
2022-01-08 19:08:14 -0500

    Updating Areagrab

--- Official/Panel-Altering Chips/areagrab/entry.lua
+++ Official/Panel-Altering Chips/areagrab/entry.lua
@@ -3,7 +3,7 @@ nonce = function() end
 local AUDIO = Engine.load_audio(_modpath.."sfx.ogg")
 local FINISH_AUDIO = Engine.load_audio(_modpath.."finish_sfx.ogg")
 local TEXTURE = Engine.load_texture(_modpath.."grab.png")
-local FRAME1 = {1, 1.28}
+local FRAME1 = {1, 1.3}
 local LONG_FRAME = make_frame_data({FRAME1})
 
 function package_init(package) 
@@ -28,23 +28,24 @@ function card_create_action(actor, props)
 	action:set_lockout(make_animation_lockout())
     action.execute_func = function(self, user)
         print("in custom card action execute_func()!")
-		self.tile = nil
-		local opposite_team = Team.Blue
+		local tile = nil
 		if user:get_team() == Team.Red then
 			if user:get_facing() == Direction.Right then
-				self.tile = user:get_field():tile_at(1, 2)
+				tile = user:get_field():tile_at(1, 2)
 			else
-				self.tile = user:get_field():tile_at(6, 2)
+				tile = user:get_field():tile_at(6, 2)
 			end
 		else
-			opposite_team = Team.Red
-			self.tile = user:get_field():tile_at(6, 2)
+			if user:get_facing() == Direction.Left then
+				tile = user:get_field():tile_at(6, 2)
+			else
+				tile = user:get_field():tile_at(1, 2)
+			end
 		end
-		self.dir = user:get_facing()
-		self.tile_array = {nil, nil, nil}
-		self.count = 1
-		self.max = 6
-		local ref = self
+		local dir = user:get_facing()
+		local tile_array = {}
+		local count = 1
+		local max = 6
 		local tile_front = nil
 		local tile_up = nil
 		local tile_down = nil
@@ -53,48 +54,33 @@ function card_create_action(actor, props)
 		local check_up = false
 		local check_down = false
 		
-		local fx1 = nil
-		local fx2 = nil
-		local fx3 = nil
-		for i = ref.count, ref.max, 1 do
+		for i = count, max, 1 do
 			
-			tile_front = ref.tile:get_tile(user:get_facing(), i)
+			tile_front = tile:get_tile(dir, i)
 			tile_up = tile_front:get_tile(Direction.Up, 1)
 			tile_down = tile_front:get_tile(Direction.Down, 1)
 			
-			check_front = tile_front and not user:is_team(tile_front:get_team()) and not tile_front:is_edge() and not tile_front:get_team() ~= Team.Other and tile_front:get_tile(user:get_facing(), 1):get_team() == opposite_team
-			check_up = tile_up and not user:is_team(tile_up:get_team()) and not tile_up:is_edge() and not tile_up:get_team() ~= Team.Other and tile_up:get_tile(user:get_facing(), 1):get_team() == opposite_team
-			check_down = tile_down and not user:is_team(tile_down:get_team()) and not tile_down:is_edge() and not tile_down:get_team() ~= Team.Other and tile_down:get_tile(user:get_facing(), 1):get_team() == opposite_team
+			check_front = tile_front and not user:is_team(tile_front:get_team()) and not tile_front:is_edge() and not tile_front:get_team() ~= Team.Other and user:is_team(tile_front:get_tile(Direction.reverse(dir), 1):get_team())
+			check_up = tile_up and not user:is_team(tile_up:get_team()) and not tile_up:is_edge() and not tile_up:get_team() ~= Team.Other and user:is_team(tile_up:get_tile(Direction.reverse(dir), 1):get_team())
+			check_down = tile_down and not user:is_team(tile_down:get_team()) and not tile_down:is_edge() and not tile_down:get_team() ~= Team.Other and user:is_team(tile_down:get_tile(Direction.reverse(dir), 1):get_team())
 			
 			if check_front or check_up or check_down then
-				ref.tile_array[0] = tile_front
-				if tile_up and not tile_up:is_edge() then
-					ref.tile_array[1] = tile_up
-				end
-				if tile_down and not tile_down:is_edge() then
-					ref.tile_array[2] = tile_down
-				end
+				table.insert(tile_array, tile_front)
+				table.insert(tile_array, tile_up)
+				table.insert(tile_array, tile_down)
 				break
 			end
 		end
 		
-		if #ref.tile_array > 0 and not check1 then
+		if #tile_array > 0 and not check1 then
 			Engine.play_audio(AUDIO, AudioPriority.Low)
-			if ref.tile_array[0] ~= nil then
-				fx1 = MakeTileSplash(user)
-				user:get_field():spawn(fx1, ref.tile_array[0])
-			end
-			if ref.tile_array[1] ~= nil then
-				fx2 = MakeTileSplash(user)
-				user:get_field():spawn(fx2, ref.tile_array[1])
-			end
-			if ref.tile_array[2] ~= nil then
-				fx3 = MakeTileSplash(user)
-				user:get_field():spawn(fx3, ref.tile_array[2])
+			for i = 1, #tile_array, 1 do
+				local fx = MakeTileSplash(user)
+				user:get_field():spawn(fx, tile_array[i])
 			end
 			check1 = true
 		end
-		if #ref.tile_array > 0 then
+		if #tile_array > 0 and check1 then
 			Engine.play_audio(FINISH_AUDIO, AudioPriority.Low)
 		end
 	end
@@ -107,11 +93,12 @@ function MakeTileSplash(user)
 	local anim = artifact:get_animation()
 	anim:load(_modpath.."areagrab.animation")
 	anim:set_state("FALL")
+	anim:refresh(artifact:sprite())
 	artifact:set_offset(0.0, -296.0)
 	artifact:sprite():set_layer(-1)
 	local doOnce = false
 	artifact.update_func = function(self, dt)
-		if self:get_offset().y >= -24 then
+		if self:get_offset().y >= -16 then
 			if not doOnce then
 				self:set_offset(0.0, 0.0)
 				self:get_animation():set_state("EXPAND")
@@ -134,7 +121,7 @@ function MakeTileSplash(user)
 				end
 			)
 		else
-			self:set_offset(0.0, self:get_offset().y + 8.0)
+			self:set_offset(0.0, self:get_offset().y + 16.0)
 		end
 	end
 	artifact.delete_func = function(self)
